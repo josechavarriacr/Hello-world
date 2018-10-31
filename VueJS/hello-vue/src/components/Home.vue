203bcbe3a0be42bcc7bf3d7d5f00143c05a9ccac
Jose Chavarria <jose.chavarria@grupoardigital.com>
2018-10-30 23:48:38 -0600

    axios completed

--- VueJS/hello-vue/src/components/Home.vue
+++ VueJS/hello-vue/src/components/Home.vue
@@ -1,31 +1,37 @@
 <template lang="html">
   <div class="">
     <h1>{{hello}}</h1>
-    <p>{{post.title}}</p>
+    <li v-for="item of posts">
+      <h5>greeting: {{item.greeting}}</h5>
+      <h5>server: {{item.server}}</h5>
+    </li>
+    <h2 v-for></h2>
   </div>
 </template>
 
 <script>
-import {HTTP} from '../http-common';
+import axios from 'axios';
 
 export default {
   name:'home',
   data() {
     return {
       hello: 'Hola desde vuejs',
-      post: [],
-      errors: []
+      posts: [],
+      errored: false
     }
   },
-  created() {
-      HTTP.get(`posts`)
-      .then(response => {
-        this.posts = response.data
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
-    }
+  mounted () {
+     axios
+       .get('http://chavarria.cr:1337/tests')
+       .then(response => {
+         this.posts= response.data
+       })
+       .catch(error => {
+         this.errored = true
+       })
+       .finally(() => this.loading = false)
+   }
 }
 </script>
 
--- VueJS/hello-vue/src/http-common.js
+++ VueJS/hello-vue/src/http-common.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `http://jsonplaceholder.typicode.com/`,
-  headers: {
-    Authorization: 'Bearer {token}'
-  }
+  baseURL: `'https://api.coindesk.com/v1/bpi/currentprice.json`,
 })
