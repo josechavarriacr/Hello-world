dd3d15d821aa2855ccf8a60045ff4d6eaf31e826
Jose Chavarria <jose.chavarria@grupoardigital.com>
2018-10-30 23:34:30 -0600

    create new component with axios for http-get api

---
+++ VueJS/hello-vue/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="">
+    <h1>{{hello}}</h1>
+    <p>{{post.title}}</p>
+  </div>
+</template>
+
+<script>
+import {HTTP} from '../http-common';
+
+export default {
+  name:'home',
+  data() {
+    return {
+      hello: 'Hola desde vuejs',
+      post: [],
+      errors: []
+    }
+  },
+  created() {
+      HTTP.get(`posts`)
+      .then(response => {
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+}
+</script>
+
+<style lang="css">
+</style>
